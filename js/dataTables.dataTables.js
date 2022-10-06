57a4695a8f66db5e5bce91f54d1d1fd4f5f8062a
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-10-06 22:02:37 +0100

    4742966f21cd61a8a7aba73fac9ead899406d477 Build: Correctly build the integration files for the new ESM / UMD build
    dc89d1779f9692173916144078a495955bb860fc Fix: Types - Golobal `new DataTable()` construction was incorrectly typed
    
    Sync to source repo @dc89d1779f9692173916144078a495955bb860fc

--- js/dataTables.dataTables.js
+++ js/dataTables.dataTables.js
@@ -6,7 +6,7 @@
 (function( factory ){
 	if ( typeof define === 'function' && define.amd ) {
 		// AMD
-		define( [''], function ( $ ) {
+		define( ['jquery', 'datatables.net'], function ( $ ) {
 			return factory( $, window, document );
 		} );
 	}
@@ -19,8 +19,14 @@
 				root = window;
 			}
 
+			if ( ! $ ) {
+				$ = typeof window !== 'undefined' ? // jQuery's factory checks for a global window
+					require('jquery') :
+					require('jquery')( root );
+			}
+
 			if ( ! $.fn.dataTable ) {
-				require('')(root, $);
+				require('datatables.net')(root, $);
 			}
 
 
